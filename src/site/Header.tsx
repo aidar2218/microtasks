6a0e4068c9fd67bbbdfbd1ae9dbb920593f379b6
Aidar Ibraev <aidaribraev32@gmail.com>
2024-03-22 19:05:13 +0600

    Commit 1.1.1. Deleted styles from App.css. Add new components.

---
+++ src/site/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+type HeaderPropsType = {
+    titleForHeader: string
+}
+
+export const Header = (props: HeaderPropsType) => {
+    return (
+        <div>
+            {props.titleForHeader}
+        </div>
+    );
+};
+
