169367b450e97b94a80ac8e1defd6b8923beaccf
Aidar Ibraev <aidaribraev32@gmail.com>
2024-03-27 09:15:47 +0600

    Commit 1.1.4. Worked with hook useState();

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,32 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import './App.css';
 import {Button} from "./components/Button";
 
 function App() {
 
-    // const firstSubscriber = (event: number) => {
-    //     console.log("hello i'm Aidar")
-    // }
-    //
-    // const secondSubscriber = () => {
-    //     console.log("hello i'm Ivan")
-    // }
+    let [num, setNum] = useState(0);
 
-    // const onClickHandler = (name: string) => {
-    //     console.log(`hello i'm ${name}`)
-    // }
+    const increment = () => {
+        setNum(num + 1);
+    }
 
-    // const firstFunc = () => {
-    //     console.log(101);
-    // }
-    //
-    // const secondFunc = (num: number) => {
-    //     console.log(num);
-    // }
+    const decrement = () => {
+        setNum(num - 1)
+    }
 
-    const buttonFunc = (name?: string, age?: number, address?: string) => {
-        if (name && age && address) {
-            return console.log(`Hi, my name is ${name}, I'm ${age} years old, I live in ${address}`);
-        }
-        if (name && age) {
-            return console.log(`Hi, my name is ${name}, I'm ${age} years old`);
-        }
-        if (name) {
-            return console.log(`Hi I'm ${name}`);
-        }
-        console.log("I'm stupid button")
+    const clearNum = () => {
+        setNum(num = 0);
     }
 
     return (
-        <div>
-            {/*<button onClick={(event) => onClickHandler("Vasya")}>button1</button>*/}
-            {/*<button onClick={() => onClickHandler("Ivan")}>button2</button>*/}
+        <div className={"App"}>
+            <h1>{num}</h1>
+            <div className={"buttons"}>
+                <button onClick={increment}>+</button>
+                <button onClick={decrement}>-</button>
+                <button onClick={clearNum}>clear</button>
+            </div>
 
-            {/*<button onClick={firstFunc}>1</button>*/}
-            {/*<button onClick={() => secondFunc(11)}>2</button>*/}
-
-
-
-            <Button name={"BUTTON1"} callback={() => buttonFunc("Vasya")}/>
-            <Button name={"BUTTON2"} callback={() => buttonFunc("Ivan", 24)}/>
-            <Button name={"BUTTON3"} callback={() => buttonFunc("Dima", 33, "Moscow")}/>
-            <Button name={"Stupid button"} callback={() => buttonFunc()}/>
         </div>
     );
 }
