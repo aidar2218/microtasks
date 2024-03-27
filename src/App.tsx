12fc0042dda80664dbb68ebc70c5202d0017d0dd
Aidar Ibraev <aidaribraev32@gmail.com>
2024-03-27 08:48:21 +0600

    Commit 1.1.3. Worked with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
 import React from 'react';
 import './App.css';
-import {NewComponent} from "./site/NewComponent";
+import {Button} from "./components/Button";
 
 function App() {
-    const students = [
-        {id: 1, name: "James", age: 8},
-        {id: 2, name: "Robert", age: 18},
-        {id: 3, name: "John", age: 28},
-        {id: 4, name: "Michael", age: 38},
-        {id: 5, name: "William", age: 48},
-        {id: 6, name: "David", age: 58},
-        {id: 7, name: "Richard", age: 68},
-        {id: 8, name: "Joseph", age: 78},
-        {id: 9, name: "Thomas", age: 88},
-        {id: 10, name: "Charles", age: 98},
-        {id: 11, name: "Christopher", age: 100},
-    ]
+
+    // const firstSubscriber = (event: number) => {
+    //     console.log("hello i'm Aidar")
+    // }
+    //
+    // const secondSubscriber = () => {
+    //     console.log("hello i'm Ivan")
+    // }
+
+    // const onClickHandler = (name: string) => {
+    //     console.log(`hello i'm ${name}`)
+    // }
+
+    // const firstFunc = () => {
+    //     console.log(101);
+    // }
+    //
+    // const secondFunc = (num: number) => {
+    //     console.log(num);
+    // }
+
+    const buttonFunc = (name?: string, age?: number, address?: string) => {
+        if (name && age && address) {
+            return console.log(`Hi, my name is ${name}, I'm ${age} years old, I live in ${address}`);
+        }
+        if (name && age) {
+            return console.log(`Hi, my name is ${name}, I'm ${age} years old`);
+        }
+        if (name) {
+            return console.log(`Hi I'm ${name}`);
+        }
+        console.log("I'm stupid button")
+    }
 
     return (
         <div>
-            <NewComponent students={students} />
+            {/*<button onClick={(event) => onClickHandler("Vasya")}>button1</button>*/}
+            {/*<button onClick={() => onClickHandler("Ivan")}>button2</button>*/}
+
+            {/*<button onClick={firstFunc}>1</button>*/}
+            {/*<button onClick={() => secondFunc(11)}>2</button>*/}
+
+
+
+            <Button name={"BUTTON1"} callback={() => buttonFunc("Vasya")}/>
+            <Button name={"BUTTON2"} callback={() => buttonFunc("Ivan", 24)}/>
+            <Button name={"BUTTON3"} callback={() => buttonFunc("Dima", 33, "Moscow")}/>
+            <Button name={"Stupid button"} callback={() => buttonFunc()}/>
         </div>
     );
 }
